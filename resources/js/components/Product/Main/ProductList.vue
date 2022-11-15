e79ab496b0c9d3574da97fed6b9c17efa03ef356
pedro.darde <pedro.darde@multi24h.com.br>
2022-11-15 16:17:41 -0300

    removendo v-if

--- resources/js/components/Product/Main/ProductList.vue
+++ resources/js/components/Product/Main/ProductList.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="container Produtos mt-2" v-if="products.length">
-        <h3 class="text-center text-uppercase"> Linha {{ line }}</h3>
+    <div class="container Produtos mt-2">
+        <h3 class="text-center text-uppercase">Linha {{ line }}</h3>
         <div class="d-flex flex-row flex-wrap justify-content-center">
             <div v-for="product in products" :key="product.id">
                 <product
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,6 +5,6 @@
 @endsection
 
 @section('content')
-    <product-list :products="{{ $products }}" line="{{ $line }}">
-    </product-lis>
-@endsection
+    <product-list :products=" {{ $products }} " line="{{ $line }}">
+        </product-lis>
+    @endsection
